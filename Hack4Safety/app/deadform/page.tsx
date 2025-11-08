d13c3f3778bc7ac96298e184c97a35646fdc73ed
Ashwani Senapati <senapatiashwani47@gmail.com>
2025-11-09 03:59:13 +0530

    update

--- Hack4Safety/app/deadform/page.tsx
+++ Hack4Safety/app/deadform/page.tsx
@@ -260,7 +260,7 @@ export default function AddDeadPersonForm() {
                     name="reportFiledBy.name"
                     value={formData.reportFiledBy.name}
                     onChange={handleChange}
-                    className="w-full px-4 py-2 border border-slate-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+                    className="w-full px-4 py-2 border text-black border-slate-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent"
                   />
                 </div>
                 <div>
@@ -270,7 +270,7 @@ export default function AddDeadPersonForm() {
                     name="reportFiledBy.designation"
                     value={formData.reportFiledBy.designation}
                     onChange={handleChange}
-                    className="w-full px-4 py-2 border border-slate-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+                    className="w-full px-4 py-2 border text-black border-slate-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent"
                   />
                 </div>
                 <div>
@@ -280,7 +280,7 @@ export default function AddDeadPersonForm() {
                     name="reportFiledBy.policeStation"
                     value={formData.reportFiledBy.policeStation}
                     onChange={handleChange}
-                    className="w-full px-4 py-2 border border-slate-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+                    className="w-full px-4 py-2 border text-black border-slate-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent"
                   />
                 </div>
               </div>
